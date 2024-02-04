53c502e9117d41401c77fc0b49a525bbaa135ffb
Cheessse <davidprekaska12345@gmail.com>
2024-02-04 13:45:30 +0000

    completed task-3

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 const form = document.querySelector('#task-form');
 const list = document.querySelector('.task-list');
 
+const TASK_KEY = 'taskKey';
+
 form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(e) {
@@ -8,13 +10,23 @@ function onFormSubmit(e) {
   const inputValue = e.target.elements.taskName.value.trim();
   if (inputValue === '') return;
   console.dir(inputValue);
-  
   addTaskSubmit(inputValue);
+  savetoLS(inputValue);
   e.target.reset();
 }
 
 function addTaskSubmit(inputValue) {
-
   const elementLi = `<li>${inputValue}</li>`;
-  list.insertAdjacentHTML("beforeend", elementLi);
+  list.insertAdjacentHTML('beforeend', elementLi);
+}
+
+function savetoLS(inputValue) {
+  const arr = JSON.parse(localStorage.getItem(TASK_KEY)) || [];
+
+  arr.push(inputValue);
+
+  console.log(arr);
+
+  const toLS = JSON.stringify(arr);
+  localStorage.setItem(TASK_KEY, toLS);
 }
